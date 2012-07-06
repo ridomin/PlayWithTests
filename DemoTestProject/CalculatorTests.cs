add3a631fd1ab2a2b04d18cbac2edcd5c0b0b548
rido min <rmpablos@gmail.com>
2012-07-06 13:20:02 +0200

    Demo to generate TRX

---
+++ DemoTestProject/CalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleLib;
+
+namespace DemoTestProject
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void AddOk()
+        {
+            Assert.AreEqual(4, new Calculator().Add(2, 2));
+        }
+    }
+}
